726c1803153ac856d4b5c38da1d3149846d4e79b
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-01 18:40:58 +0200

    refactor: create screens folder

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import Card from "./src/components/TinderCard";
-import { burgers } from "./public/burgers";
-import AnimatedStack from "./src/components/Animated";
+import HomeScreen from "./src/screens/HomeScreen";
 
 const App = () => {
-  const onSwipeRight = (food) => {
-    console.warn("swipe right", food.dsc);
-  };
-
-  const onSwipeLeft = (food) => {
-    console.warn("swipe left", food.dsc);
-  };
-
   return (
     <View style={styles.pageContainer}>
-      <AnimatedStack
-        onSwipeLeft={onSwipeLeft}
-        onSwipeRight={onSwipeRight}
-        data={burgers}
-        renderItem={({ item }) => <Card food={item} />}
-      />
+      <HomeScreen />
     </View>
   );
 };
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Card from "../components/TinderCard";
+import { burgers } from "../../public/burgers";
+import AnimatedStack from "../components/AnimatedStack";
+
+const HomeScreen = () => {
+  const onSwipeRight = (food) => {
+    console.warn("swipe right", food.dsc);
+  };
+
+  const onSwipeLeft = (food) => {
+    console.warn("swipe left", food.dsc);
+  };
+
+  return (
+    <View style={styles.pageContainer}>
+      <AnimatedStack
+        onSwipeLeft={onSwipeLeft}
+        onSwipeRight={onSwipeRight}
+        data={burgers}
+        renderItem={({ item }) => <Card food={item} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    width: "100%",
+  },
+});
+
+export default HomeScreen;
