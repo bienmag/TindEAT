b027e8f84eb3c7ff0db92261ed3d65eb9a621d9c
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-01 16:15:46 +0200

    feat: card animation

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { Text, Image, ImageBackground, View, StyleSheet } from "react-native";
+import "react-native-gesture-handler";
+import React from "react";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
 import Card from "./src/components/TinderCard";
 import { burgers } from "./public/burgers";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+  useAnimatedGestureHandler,
+  useDerivedValue,
+  interpolate,
+} from "react-native-reanimated";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
 
 const App = () => {
+  const { width: screenWidth } = useWindowDimensions();
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () => interpolate(translateX.value, [0, screenWidth], [0, 60]) + "deg"
+  );
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      },
+    ],
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = translateX.value;
+    },
+    onActive: (e, ctx) => {
+      translateX.value = ctx.startX + e.translationX;
+    },
+    onEnd: () => {
+      console.log("Touch end");
+    },
+  });
+
   return (
     <View style={styles.pageContainer}>
-      <Card food={burgers[0]}></Card>
+      <GestureHandlerRootView>
+        <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <Card food={burgers[0]}></Card>
+          </Animated.View>
+        </PanGestureHandler>
+      </GestureHandlerRootView>
     </View>
   );
 };
@@ -53,6 +102,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "white",
     lineHeight: 25,
+  },
+
+  animatedCard: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
