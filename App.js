98ca35025dac1f851ae3ab7426e84538b3eb1fe1
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-01 18:13:26 +0200

    refactor: clean app

--- App.js
+++ App.js
@@ -1,127 +1,17 @@
 import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, useWindowDimensions, Image } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import Card from "./src/components/TinderCard";
 import { burgers } from "./public/burgers";
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-  withTiming,
-  useAnimatedGestureHandler,
-  useDerivedValue,
-  interpolate,
-  runOnJS,
-} from "react-native-reanimated";
-import {
-  GestureHandlerRootView,
-  PanGestureHandler,
-} from "react-native-gesture-handler";
-import like from "./assets/LIKE.png";
-import nope from "./assets/nope.png";
-
-const swipe_velocity = 900;
+import AnimatedStack from "./src/components/Animated";
 
 const App = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [nextIndex, setNextIndex] = useState(1);
-
-  const currentFood = burgers[currentIndex];
-  const nextFood = burgers[nextIndex];
-
-  const { width: screenWidth } = useWindowDimensions();
-  const hiddenTranslateX = 2 * screenWidth;
-  const translateX = useSharedValue(0);
-  const rotate = useDerivedValue(
-    () => interpolate(translateX.value, [0, hiddenTranslateX], [0, 60]) + "deg"
-  );
-
-  const cardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateX: translateX.value,
-      },
-      {
-        rotate: rotate.value,
-      },
-    ],
-  }));
-
-  const nextCardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        scale: interpolate(
-          translateX.value,
-          [-hiddenTranslateX, 0, hiddenTranslateX],
-          [1, 0.8, 1]
-        ),
-      },
-    ],
-    opacity: interpolate(
-      translateX.value,
-      [-hiddenTranslateX, 0, hiddenTranslateX],
-      [1, 0.5, 1]
-    ),
-  }));
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: (_, ctx) => {
-      ctx.startX = translateX.value;
-    },
-    onActive: (e, ctx) => {
-      translateX.value = ctx.startX + e.translationX;
-    },
-    onEnd: (e) => {
-      if (Math.abs(e.velocityX) < swipe_velocity) {
-        translateX.value = withSpring(0);
-        return;
-      }
-
-      translateX.value = withSpring(
-        e.velocityX > 0 ? hiddenTranslateX : -hiddenTranslateX,
-        {},
-        () => runOnJS(setCurrentIndex)(currentIndex + 1)
-      );
-    },
-  });
-
-  const likeStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(translateX.value, [0, hiddenTranslateX / 5], [0, 1]),
-  }));
-
-  const nopeStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(translateX.value, [0, -hiddenTranslateX / 5], [0, 1]),
-  }));
-
-  useEffect(() => {
-    translateX.value = 0;
-    setNextIndex(currentIndex + 1);
-  }, [currentIndex, translateX]);
-
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.nextCardContainer}>
-        <Animated.View style={[styles.animatedCard, nextCardStyle]}>
-          <Card food={nextFood}></Card>
-        </Animated.View>
-      </View>
-      <GestureHandlerRootView style={styles.pageContainer}>
-        <PanGestureHandler onGestureEvent={gestureHandler}>
-          <Animated.View style={[styles.animatedCard, cardStyle]}>
-            <Card food={currentFood}></Card>
-            <Animated.Image
-              source={like}
-              style={[styles.like, { left: 10 }, likeStyle]}
-              resizeMode="contain"
-            ></Animated.Image>
-            <Animated.Image
-              source={nope}
-              style={[styles.like, { right: 10 }, nopeStyle]}
-              resizeMode="contain"
-            ></Animated.Image>
-          </Animated.View>
-        </PanGestureHandler>
-      </GestureHandlerRootView>
+      <AnimatedStack
+        data={burgers}
+        renderItem={({ item }) => <Card food={item} />}
+      />
     </View>
   );
 };
@@ -132,25 +22,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
     width: "100%",
-  },
-
-  animatedCard: {
-    width: "100%",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  nextCardContainer: {
-    ...StyleSheet.absoluteFillObject,
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  like: {
-    width: 150,
-    height: 150,
-    position: "absolute",
-    top: 120,
   },
 });
 
---
+++ src/components/Animated/index.js
@@ -0,0 +1,156 @@
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  useAnimatedGestureHandler,
+  useDerivedValue,
+  interpolate,
+  runOnJS,
+} from "react-native-reanimated";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import like from "../../../assets/LIKE.png";
+import nope from "../../../assets/nope.png";
+
+const swipe_velocity = 900;
+
+const AnimatedStack = (props) => {
+  const { data, renderItem } = props;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(1);
+
+  const currentFood = data[currentIndex];
+  const nextFood = data[nextIndex];
+
+  const { width: screenWidth } = useWindowDimensions();
+  const hiddenTranslateX = 2 * screenWidth;
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () => interpolate(translateX.value, [0, hiddenTranslateX], [0, 60]) + "deg"
+  );
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      },
+    ],
+  }));
+
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.8, 1]
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.5, 1]
+    ),
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = translateX.value;
+    },
+    onActive: (e, ctx) => {
+      translateX.value = ctx.startX + e.translationX;
+    },
+    onEnd: (e) => {
+      if (Math.abs(e.velocityX) < swipe_velocity) {
+        translateX.value = withSpring(0);
+        return;
+      }
+
+      translateX.value = withSpring(
+        e.velocityX > 0 ? hiddenTranslateX : -hiddenTranslateX,
+        {},
+        () => runOnJS(setCurrentIndex)(currentIndex + 1)
+      );
+    },
+  });
+
+  const likeStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(translateX.value, [0, hiddenTranslateX / 5], [0, 1]),
+  }));
+
+  const nopeStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(translateX.value, [0, -hiddenTranslateX / 5], [0, 1]),
+  }));
+
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
+
+  return (
+    <View style={styles.pageContainer}>
+      <View style={styles.nextCardContainer}>
+        <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+          {renderItem({ item: nextFood })}
+        </Animated.View>
+      </View>
+      <GestureHandlerRootView style={styles.pageContainer}>
+        <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            {renderItem({ item: currentFood })}
+            <Animated.Image
+              source={like}
+              style={[styles.like, { left: 10 }, likeStyle]}
+              resizeMode="contain"
+            ></Animated.Image>
+            <Animated.Image
+              source={nope}
+              style={[styles.like, { right: 10 }, nopeStyle]}
+              resizeMode="contain"
+            ></Animated.Image>
+          </Animated.View>
+        </PanGestureHandler>
+      </GestureHandlerRootView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    width: "100%",
+  },
+
+  animatedCard: {
+    width: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  like: {
+    width: 150,
+    height: 150,
+    position: "absolute",
+    top: 120,
+  },
+});
+
+export default AnimatedStack;
