f9a9bedfd82857e19573c39b79bd6ad9948ab70b
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-01 12:30:37 +0200

    tindeat card ui

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { Text, Image, ImageBackground, View, StyleSheet } from "react-native";
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.pageContainer}>
+      <View style={styles.card}>
+        <ImageBackground
+          source={require("./assets/pizza.jpeg")}
+          style={styles.image}
+        >
+          <View style={styles.cardInner}>
+            <Text style={styles.name}>Pizza</Text>
+            <Text style={styles.bio}>Pizza Margarita</Text>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  pageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  card: {
+    width: "95%",
+    height: "70%",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    overflow: "hidden",
+    justifyContent: "flex-end",
+  },
+
+  cardInner: {
+    padding: 10,
+  },
+
+  name: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+  },
+
+  bio: {
+    fontSize: 18,
+    color: "white",
+    lineHeight: 25,
   },
 });
+
+export default App;
