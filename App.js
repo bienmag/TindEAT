46364e2f6b36a6f2623ab002aac31aded342dde0
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-02 13:02:51 +0200

    feat: very primitive navigation between screens

--- App.js
+++ App.js
@@ -1,29 +1,45 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { View, StyleSheet, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, SafeAreaView, Pressable } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import MatchesScreen from "./src/screens/MatchesScreen";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-
 import { Fontisto } from "@expo/vector-icons";
-
 import { Ionicons } from "@expo/vector-icons";
 
 const App = () => {
+  const [activeScreen, setActiveScreen] = useState("home");
+
+  const color = "#B5B5B5";
+  const activeColor = "#F76C6B";
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.topNavigation}>
-        <Fontisto name="tinder" size={30} color="gray" />
+        <Pressable onPress={() => setActiveScreen("home")}>
+          <Fontisto
+            name="tinder"
+            size={30}
+            color={activeScreen === "home" ? activeColor : color}
+          />
+        </Pressable>
         <MaterialCommunityIcons
           name="star-four-points"
           size={30}
-          color="gray"
+          color={color}
         />
-        <Ionicons name="chatbubbles-sharp" size={30} color="gray" />
-        <Ionicons name="ios-person" size={30} color="gray" />
+        <Pressable onPress={() => setActiveScreen("matches")}>
+          <Ionicons
+            name="chatbubbles-sharp"
+            size={30}
+            color={activeScreen === "matches" ? activeColor : color}
+          />
+        </Pressable>
+        <Ionicons name="ios-person" size={30} color={color} />
       </View>
       <View style={styles.pageContainer}>
-        <HomeScreen />
+        {activeScreen === "home" && <HomeScreen />}
+        {activeScreen === "matches" && <MatchesScreen />}
       </View>
     </SafeAreaView>
   );
